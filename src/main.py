cbc27d3b48e8e1964affb79de14654a68f69fd4c
ruzanv <ruzzanv@gmail.com>
2023-12-14 16:09:05 +0400

    Добавлены тесты для файла main функции summ

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 import re
 
 
-min_leght = 250
+min_leght = 1000
 
 
 
@@ -14,10 +14,7 @@ def summ(articleInput, min_length_of_article, max_length_of_article):
                                                      max_length=max_length_of_article, 
                                                      do_sample=False)
         return summarizedArticle[0]['summary_text']
-    return f'Минимальное колличество символов в статье {min_leght}'
-
-
-
+    return f'Минимальное количество символов в статье {min_leght}'
 
 
 
@@ -26,9 +23,9 @@ summarizer_pipline = pipeline('summarization', model="d0rj/rut5-base-summ")
 demo = gr.Interface(fn=summ, 
                      title="Суммаризация текста",
                      inputs=[
-                         gr.Textbox(lines=20, placeholder="Введите статью для того чтобы сократить ее", label="Ввод текста", interactive=True),
-                         gr.Slider(10, 100, value=10, step=10, label="Минимальное колличество слов в сокращении", info="Выберите от 10 до 100"),
-                         gr.Slider(120, 250, value=120, step=10, label="Максимальное колличество слов в сокращении", info="Выберите от 120 and 250"),
+                         gr.Textbox(lines=20, placeholder="Введите статью для ее сокращения", label="Ввод текста", interactive=True),
+                         gr.Slider(50, 250, value=10, step=10, label="Минимальное колличество слов в сокращенном варианте", info="Выберите от 50 до 250"),
+                         gr.Slider(300, 1000, value=120, step=10, label="Максимальное колличество слов в сокращенном варианте", info="Выберите от 300 and 1000"),
                      ],
                      outputs=gr.Textbox(lines=20, label="Выход", show_copy_button=True),
                      allow_flagging='never')
