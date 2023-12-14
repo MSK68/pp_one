8cafe405e4607e166d14ff5fef71bee2a7d4dc01
ruzanv <ruzzanv@gmail.com>
2023-12-14 16:29:17 +0400

    Добавлены тесты для файла main функции summ

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def summ(articleInput, min_length_of_article, max_length_of_article):
                                                      max_length=max_length_of_article, 
                                                      do_sample=False)
         return summarizedArticle[0]['summary_text']
-    return f'Минимальное колличество символов в статье {min_leght}'
+    return f'Минимальное количество символов в статье {min_leght}'
 
 
 
@@ -26,9 +26,9 @@ summarizer_pipline = pipeline('summarization', model="d0rj/rut5-base-summ")
 demo = gr.Interface(fn=summ, 
                      title="Суммаризация текста",
                      inputs=[
-                         gr.Textbox(lines=20, placeholder="Введите статью для того чтобы сократить ее", label="Ввод текста", interactive=True),
-                         gr.Slider(10, 100, value=10, step=10, label="Минимальное колличество слов в сокращении", info="Выберите от 10 до 100"),
-                         gr.Slider(120, 250, value=120, step=10, label="Максимальное колличество слов в сокращении", info="Выберите от 120 and 250"),
+                         gr.Textbox(lines=20, placeholder="Введите статью ее сокращения", label="Ввод текста", interactive=True),
+                         gr.Slider(10, 100, value=10, step=10, label="Минимальное количество слов в сокращенном виде", info="Выберите от 10 до 100"),
+                         gr.Slider(120, 250, value=120, step=10, label="Максимальное количество слов в сокращенном виде", info="Выберите от 120 and 250"),
                      ],
                      outputs=gr.Textbox(lines=20, label="Выход", show_copy_button=True),
                      allow_flagging='never')
--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,32 @@
 import unittest
-from pp_one.src import main
+from pp_one.src.main import summ, min_leght
+from unittest.mock import patch
+from io import StringIO
 
-class MainTests(unittest.TestCase):
+
+class Test_Summ(unittest.TestCase):
+
+    def test_summ_short(self):
+        result = summ("Маленькая статья", 100, 200)
+        self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+
+    def test_summ_long(self):
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            result = summ("Очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень Очень очень очень очень очень очень очень очень очень большая статья", 50,
+                          200)
+            self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+            self.assertEqual(fake_output.getvalue(), '')
+
+    def test_summ_invalid_min_length(self):
+        result = summ("Проверка некорректно заданной минимальной длины",2000, 300)
+        self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+
+    def test_summ_invalid_max_length(self):
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            result = summ("Проверка некорректно заданной максимальной длины", 50,5000)
+            self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+            self.assertEqual(fake_output.getvalue(), '')
+
+
+if __name__ == '__main__':
+    unittest.main()
