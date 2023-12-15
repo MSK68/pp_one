1fdc9527e12535c6cfd842a054f0e4f256d68326
ruzanv <ruzzanv@gmail.com>
2023-12-15 14:47:17 +0400

    Добавление тестов по tg 2.0

--- test/test_tg2.py
+++ test/test_tg2.py
@@ -4,24 +4,24 @@ from pp_one.src.tg import start, response_to_gradio, requests
 import aiohttp
 
 
-class TestBotFunctions(unittest.IsolatedAsyncioTestCase):
+class Test_TG_Bot_2(unittest.IsolatedAsyncioTestCase):
     async def test_start(self):
         update = MagicMock()
-        update.effective_user.first_name = 'TestUser'
+        update.effective_user.first_name = 'FirstName'
         update.message.reply_text = AsyncMock()
         context = MagicMock()
         await start(update, context)
 
     async def test_response_to_gradio(self):
         update = MagicMock()
-        update.message.text = 'Test message'
+        update.message.text = 'Text'
         update.message.reply_html = AsyncMock()
         context = MagicMock()
-        response = {"data": "Test data", "duration": "Test duration"}
+        response = {"data": "Data something", "duration": "Duration something"}
 
-        mock_resp = AsyncMock()
-        mock_resp.json = AsyncMock(return_value=response)
-        mock_post = AsyncMock(return_value=mock_resp)
+        mock_res = AsyncMock()
+        mock_res.json = AsyncMock(return_value=response)
+        mock_post = AsyncMock(return_value=mock_res)
 
         with patch('aiohttp.ClientSession.post', new=AsyncMock(return_value=mock_resp)):
             await response_to_gradio(update, context)
