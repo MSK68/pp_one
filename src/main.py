b74ac6e34c094fd4e687da410e5e583527cdf937
Sergei Vasilev <sega.vasiev@gmail.com>
2023-12-15 22:11:23 +1100

    Добавлены комментарии в скрипты ./src/.py

--- src/main.py
+++ src/main.py
@@ -1,38 +1,46 @@
+"""
+Приложение для сокращения текста. В качестве модели используется модель d0rj/rut5-base-summ.
+Приложение реализовано с помощью библиотеки gradio.
+"""
+
 import gradio as gr
 from transformers import pipeline
 import re
 
-
 min_leght = 250
 
 
-
 def summ(articleInput, min_length_of_article, max_length_of_article):
-    if len(articleInput) > min_leght :
+    """
+    Функция для сокращения текста. В качестве модели используется модель d0rj/rut5-base-summ.
+    :param articleInput: Входной текст, string
+    :param min_length_of_article: Минимальное количество слов в сокращенном виде, int
+    :param max_length_of_article: Максимальное количество слов в сокращенном виде, int
+    :return: Сокращенный текст, string
+    """
+    if len(articleInput) > min_leght:
         summarizedArticle = summarizer_pipline(articleInput,
-                                                     min_length=min_length_of_article,
-                                                     max_length=max_length_of_article,
-                                                     do_sample=False)
+                                               min_length=min_length_of_article,
+                                               max_length=max_length_of_article,
+                                               do_sample=False)
         return summarizedArticle[0]['summary_text']
     return f'Минимальное количество символов в статье {min_leght}'
 
 
-
-
-
-
-summarizer_pipline = pipeline('summarization', model="d0rj/rut5-base-summ")
+summarizer_pipline = pipeline('summarization', model="d0rj/rut5-base-summ")  # Загрузка модели
 
 demo = gr.Interface(fn=summ,
-                     title="Суммаризация текста",
-                     inputs=[
-                         gr.Textbox(lines=20, placeholder="Введите статью ее сокращения", label="Ввод текста", interactive=True),
-                         gr.Slider(10, 100, value=10, step=10, label="Минимальное количество слов в сокращенном виде", info="Выберите от 10 до 100"),
-                         gr.Slider(120, 250, value=120, step=10, label="Максимальное количество слов в сокращенном виде", info="Выберите от 120 and 250"),
-                     ],
-                     outputs=gr.Textbox(lines=20, label="Выход", show_copy_button=True),
-                     allow_flagging='never')
+                    title="Суммаризация текста",
+                    inputs=[
+                        gr.Textbox(lines=20, placeholder="Введите статью ее сокращения", label="Ввод текста",
+                                   interactive=True),
+                        gr.Slider(10, 100, value=10, step=10, label="Минимальное количество слов в сокращенном виде",
+                                  info="Выберите от 10 до 100"),
+                        gr.Slider(120, 250, value=120, step=10, label="Максимальное количество слов в сокращенном виде",
+                                  info="Выберите от 120 and 250"),
+                    ],
+                    outputs=gr.Textbox(lines=20, label="Выход", show_copy_button=True),
+                    allow_flagging='never')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=7860)
--- src/tg.py
+++ src/tg.py
@@ -1,3 +1,7 @@
+"""
+Телеграм бот для обработки текста с помощью Gradio.
+"""
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 import requests
@@ -9,8 +13,13 @@ HOST = '127.0.0.1'
 PORT = '7860'
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Обработчик команды /start
+    :param update:
+    :param context:
+    :return:
+    """
     message = ''
     message += f'Привет {update.effective_user.first_name}\n'
     message += 'Это бот для помощи студентам с конспектами\n'
@@ -19,23 +28,28 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(message)
 
 
-
 async def response_to_gradio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Обработчик текста. Отправляет текст в Gradio и возвращает результат.
+    :param update:
+    :param context:
+    :return:
+    """
     response = requests.post(f"http://{HOST}:{PORT}/run/predict", json={
-	"data": [
-		update.message.text,
-		50,
-		250,
-	]
+        "data": [
+            update.message.text,
+            50,
+            250,
+        ]
     }).json()
     message = f'{response["data"]} \nВремя обработки запроса {response["duration"]}'
+
     await update.message.reply_html(message)
 
 
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()  # Создаем приложение
+    app.add_handler(CommandHandler("start", start, ~filters.ChatType.GROUP))  # Добавляем обработчик команды /start
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, response_to_gradio))  # Добавляем обработчик текста
 
-if __name__=="__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start, ~filters.ChatType.GROUP))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, response_to_gradio))
-
-    app.run_polling()
+    app.run_polling()  # Запускаем приложение
