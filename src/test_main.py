fedc9f7ee7cab486918945753f59f29640bf6f0c
Dmitry Dolgov <68012078+Savirman@users.noreply.github.com>
2024-03-30 16:41:36 +0900

    Update test_main.py
    Add MIN_LENGTH_DEFAULT

--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import summarize_text, min_leght
+from main import summarize_text, MIN_LENGTH_DEFAULT
 from unittest.mock import patch
 from io import StringIO
 
@@ -8,23 +8,23 @@ class Test_Summ(unittest.TestCase):
 
     def test_summarize_text_short(self):
         result = summarize_text("Маленькая статья", 100, 200)
-        self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+        self.assertEqual(result, f'Минимальное количество символов в статье {MIN_LENGTH_DEFAULT}')
 
     def test_summarize_text_long(self):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             result = summarize_text("Очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень очень Очень очень очень очень очень очень очень очень очень большая статья", 50,
                           200)
-            self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+            self.assertEqual(result, f'Минимальное количество символов в статье {MIN_LENGTH_DEFAULT}')
             self.assertEqual(fake_output.getvalue(), '')
 
     def test_summarize_text_invalid_min_length(self):
         result = summarize_text("Проверка некорректно заданной минимальной длины",2000, 300)
-        self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+        self.assertEqual(result, f'Минимальное количество символов в статье {MIN_LENGTH_DEFAULT}')
 
     def test_summarize_text_invalid_max_length(self):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             result = summarize_text("Проверка некорректно заданной максимальной длины", 50,5000)
-            self.assertEqual(result, f'Минимальное количество символов в статье {min_leght}')
+            self.assertEqual(result, f'Минимальное количество символов в статье {MIN_LENGTH_DEFAULT}')
             self.assertEqual(fake_output.getvalue(), '')
 
 
